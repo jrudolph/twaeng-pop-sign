9f117e149d9e6c7e2bb2cc6fa82d21170e6d606e
Johannes Rudolph <johannes.rudolph@gmail.com>
2021-09-27 16:48:41 +0200

    cleanup some old stuff

--- ws2812.c
+++ ws2812.c
@@ -26,54 +26,6 @@ static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
             (uint32_t) (b);
 }
 
-void pattern_snakes(uint len, uint t) {
-    for (uint i = 0; i < len; ++i) {
-        uint x = (i + (t >> 1)) % 64;
-        if (x < 10)
-            put_pixel(urgb_u32(0xff, 0, 0));
-        else if (x >= 15 && x < 25)
-            put_pixel(urgb_u32(0, 0xff, 0));
-        else if (x >= 30 && x < 40)
-            put_pixel(urgb_u32(0, 0, 0xff));
-        else
-            put_pixel(0);
-    }
-}
-
-void pattern_random(uint len, uint t) {
-    if (t % 8)
-        return;
-    for (int i = 0; i < len; ++i)
-        put_pixel(rand());
-}
-
-void pattern_sparkle(uint len, uint t) {
-    if (t % 20)
-        return;
-    for (int i = 0; i < len; ++i)
-        put_pixel(rand() % 32 ? 0 : COLOR_BRG(255,130,80));
-}
-
-void pattern_greys(uint len, uint t) {
-    int max = 32; // let's not draw too much current!
-    t %= max;
-    for (int i = 0; i < len; ++i) {
-        put_pixel(t * 0x10101);
-        if (++t >= max) t = 0;
-    }
-}
-
-typedef void (*pattern)(uint len, uint t);
-const struct {
-    pattern pat;
-    const char *name;
-} pattern_table[] = {
-        //{pattern_snakes,  "Snakes!"},
-        //{pattern_random,  "Random data"},
-        {pattern_sparkle, "Sparkles"},
-        //{pattern_greys,   "Greys"},
-};
-
 const int PIN_TX = 28;
 #define num_leds 38
 
@@ -600,15 +552,4 @@ int main() {
             sleep_ms(1);
         }
     }
-        
-    //     // int pat = rand() % count_of(pattern_table);
-    //     // int dir = (rand() >> 30) & 1 ? 1 : -1;
-    //     // puts(pattern_table[pat].name);
-    //     // puts(dir == 1 ? "(forward)" : "(backward)");
-    //     // for (int i = 0; i < 1000; ++i) {
-    //     //     pattern_table[pat].pat(num_leds, t);
-    //     //     sleep_ms(10);
-    //     //     t += dir;
-    //     // }
-    // }
 }
