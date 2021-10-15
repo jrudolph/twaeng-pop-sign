c3e0c357ab47ff80a5e55f3a11d9f43677b6f11d
Johannes Rudolph <johannes.rudolph@gmail.com>
2021-10-15 19:40:09 +0200

    setup PWM fading

--- twaeng_pop.c
+++ twaeng_pop.c
@@ -10,6 +10,8 @@
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
+#include "hardware/irq.h"
+#include "hardware/pwm.h"
 #include "ws2812.pio.h"
 
 #define COLOR_BRG(R, G, B) (((G) << 16) | ((R) << 8) | (B))
@@ -508,10 +510,47 @@ void glowing_letters() {
     }
 }
 
+#define LED_PWM_PIN 22
+
+int pwm_led_fade = 100;
+
+void on_pwm_wrap() {
+    // Clear the interrupt flag that brought us here
+    pwm_clear_irq(pwm_gpio_to_slice_num(LED_PWM_PIN));
+
+    // Square the fade value to make the LED's brightness appear more linear
+    // Note this range matches with the wrap value
+    pwm_set_gpio_level(LED_PWM_PIN, pwm_led_fade * pwm_led_fade);
+}
+
+void init_pwm() {
+        // Tell the LED pin that the PWM is in charge of its value.
+    gpio_set_function(LED_PWM_PIN, GPIO_FUNC_PWM);
+    // Figure out which slice we just connected to the LED pin
+    uint slice_num = pwm_gpio_to_slice_num(LED_PWM_PIN);
+
+    // Mask our slice's IRQ output into the PWM block's single interrupt line,
+    // and register our interrupt handler
+    pwm_clear_irq(slice_num);
+    pwm_set_irq_enabled(slice_num, true);
+    irq_set_exclusive_handler(PWM_IRQ_WRAP, on_pwm_wrap);
+    irq_set_enabled(PWM_IRQ_WRAP, true);
+
+    // Get some sensible defaults for the slice configuration. By default, the
+    // counter is allowed to wrap over its maximum range (0 to 2**16-1)
+    pwm_config config = pwm_get_default_config();
+    // Set divider, reduces counter clock to sysclock/this value
+    pwm_config_set_clkdiv(&config, 4.f);
+    // Load the configuration into our PWM slice, and set it running.
+    pwm_init(slice_num, &config, true);
+}
+
 int main() {
     //set_sys_clock_48();
     stdio_init_all();
     puts("WS2812 Smoke Test");
+
+    init_pwm();
 
     // todo get free sm
     PIO pio = pio0;
